523a27a6bee7cb18afa42705a1015e72d4c74b27
Alex Pimenta <aspepper@hotmail.com>
2025-09-13 06:26:41 -0300

    feat: render correct home layout based on session

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { FigmaScreen } from "@/components/FigmaScreen";
-import screen from "@/docs/figma_pages/home.json";
+import homeScreen from "@/docs/figma_pages/home.json";
+import loggedScreen from "@/docs/figma_pages/home-logado.json";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const screen = session ? loggedScreen : homeScreen;
   return <FigmaScreen data={screen as any} />;
 }

f6c39e5b16b1f82f24f500442cce71a895141b78
aspepper <aspepper@hotmail.com>
2025-09-16 13:22:56 -0300

    Ajustes para resolver prisma não inicializa

---
+++ lib/prisma.ts
@@ -0,0 +1,12 @@
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    // log: ['query', 'error', 'warn'] // útil pra depurar em dev
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma

7fd97f0915eed19a5384807fe7f65441d7f37e70
Alex Pimenta <aspepper@hotmail.com>
2025-09-18 07:18:09 -0300

    Ensure Prisma engines are deployed and improve App Insights setup

--- lib/app-insights.ts
+++ lib/app-insights.ts
@@ -2,14 +2,24 @@ import type { TelemetryClient } from 'applicationinsights'
 
 let client: TelemetryClient | null = null
 
-if (process.env.APPINSIGHTS_CONNECTION_STRING) {
+const connectionString = [
+  'APPINSIGHTS_CONNECTION_STRING',
+  'APPLICATIONINSIGHTS_CONNECTION_STRING',
+  'APPINSIGHTS_INSTRUMENTATIONKEY',
+  'APPLICATIONINSIGHTS_INSTRUMENTATIONKEY',
+  'APPLICATIONINSIGHTS_INSTRUMENTATION_KEY',
+]
+  .map((key) => process.env[key])
+  .find((value): value is string => Boolean(value))
+
+if (connectionString) {
   try {
     const appInsights = (
       eval('require') as (moduleName: string) => unknown
     )('applicationinsights') as typeof import('applicationinsights')
 
     appInsights
-      .setup(process.env.APPINSIGHTS_CONNECTION_STRING)
+      .setup(connectionString)
       .setAutoCollectConsole(true, true)
       .start()
 

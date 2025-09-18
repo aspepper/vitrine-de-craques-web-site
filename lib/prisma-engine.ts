b80eee356f46a5ab75b3f4053cfee2d3757b91c6
Alex Pimenta <aspepper@hotmail.com>
2025-09-18 11:30:00 -0300

    Fix Prisma engine resolution for macOS

--- lib/prisma-engine.ts
+++ lib/prisma-engine.ts
@@ -1,10 +1,23 @@
 import { existsSync } from 'node:fs'
 import { join } from 'node:path'
 
-const engineFileNames = [
-  'libquery_engine-debian-openssl-1.1.x.so.node',
-  'libquery_engine-debian-openssl-3.0.x.so.node',
-]
+const engineFileNames = (() => {
+  if (process.platform === 'darwin') {
+    const darwinEngines = [
+      'libquery_engine-darwin-arm64.dylib.node',
+      'libquery_engine-darwin.dylib.node',
+    ]
+
+    return process.arch === 'arm64'
+      ? darwinEngines
+      : [...darwinEngines].reverse()
+  }
+
+  return [
+    'libquery_engine-debian-openssl-1.1.x.so.node',
+    'libquery_engine-debian-openssl-3.0.x.so.node',
+  ]
+})()
 
 const engineSearchPaths = [
   ['node_modules', '.prisma', 'client'],

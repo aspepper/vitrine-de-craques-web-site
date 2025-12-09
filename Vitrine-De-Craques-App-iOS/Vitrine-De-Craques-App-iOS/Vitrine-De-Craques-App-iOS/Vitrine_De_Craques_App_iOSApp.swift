dd10d95a66c9ecd9072b6febe335b6c3e16b7dcf
aspepper <aspepper@hotmail.com>
2025-12-08 21:00:56 -0300

    Fix: Converte submódulo fantasma em pasta normal

---
+++ Vitrine-De-Craques-App-iOS/Vitrine-De-Craques-App-iOS/Vitrine-De-Craques-App-iOS/Vitrine_De_Craques_App_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vitrine_De_Craques_App_iOSApp.swift
+//  Vitrine-De-Craques-App-iOS
+//
+//  Created by Alex Pimenta on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Vitrine_De_Craques_App_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vitrine-De-Craques-App-iOS/Vitrine-De-Craques-App-iOS/Vitrine-De-Craques-App-iOSTests/Vitrine_De_Craques_App_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Vitrine_De_Craques_App_iOSTests.swift
+//  Vitrine-De-Craques-App-iOSTests
+//
+//  Created by Alex Pimenta on 19/10/25.
+//
+
+import Testing
+
+struct Vitrine_De_Craques_App_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

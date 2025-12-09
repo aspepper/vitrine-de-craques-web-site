a82bf4dc72647e808cbc9f910c4ed3d575e08060
aspepper <aspepper@hotmail.com>
2025-12-08 22:04:06 -0300

    Tentativa de correção

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,3 @@
-import { existsSync } from 'node:fs';
-
-const prismaEngineCandidates = [
-  './node_modules/.prisma/client/libquery_engine-debian-openssl-3.0.x.so.node',
-  './node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node',
-  './node_modules/.prisma/client/libquery_engine-darwin.dylib.node',
-  './node_modules/.prisma/client/libquery_engine-darwin-arm64.dylib.node',
-  './node_modules/.prisma/client/libquery_engine-linux-musl-openssl-3.0.x.so.node',
-];
-
-const prismaEnginePaths = prismaEngineCandidates.filter((enginePath) =>
-  existsSync(enginePath),
-);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
